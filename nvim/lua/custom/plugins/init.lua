84043cd249bf8526b3480e804192cceb162106e4
Ben Verna-Asals <benjamin.asals@gmail.com>
2025-06-06 09:55:12 -0400

    Use MesloLGL font family

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -16,6 +16,7 @@ return {
     event = 'InsertEnter',
     -- stylua: ignore
     config = function()
+      vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
       vim.keymap.set('i', '<C-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
       vim.keymap.set('i', '<C-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
     end,
