02c3968f04a093b849242cccb5edc2fd0ba8c7ae
Ben Verna-Asals <benjamin.asals@gmail.com>
2025-08-28 09:49:02 -0400

    Remove tabline and customize statusline

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,6 +1,6 @@
 return {
   {
-    'echasnovski/mini.nvim',
+    'nvim-mini/mini.nvim',
     config = function()
       -- Better Around/Inside textobjects
       require('mini.ai').setup { n_lines = 500 }
@@ -8,12 +8,47 @@ return {
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
       require('mini.surround').setup()
 
-      -- Tabline that shows open buffers
-      require('mini.tabline').setup()
-
       -- Statusline
       local statusline = require 'mini.statusline'
       statusline.setup { use_icons = vim.g.have_nerd_font }
+
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_git = function()
+        return ''
+      end
+
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_diff = function()
+        return ''
+      end
+
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_diagnostics = function()
+        return ''
+      end
+
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_lsp = function()
+        return ''
+      end
+
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.inactive = function()
+        local filename = vim.fn.expand '%:t'
+        return statusline.section_filename {
+          { hl = 'MiniStatuslineFilenameInactive', strings = { filename } },
+        }
+      end
+
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_filename = function()
+        return vim.fn.expand '%:t'
+      end
+
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_fileinfo = function()
+        return vim.bo.filetype
+      end
 
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
