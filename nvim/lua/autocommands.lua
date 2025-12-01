3104b44e21d0697e98b7ebfaeb23466b97b04e02
Ben Verna-Asals <benjaminv@pixomondo.com>
2025-12-01 15:59:09 -0500

    Exclude snacks picker from cursor restoration

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -15,8 +15,15 @@ vim.api.nvim_create_autocmd('VimResized', {
 -- Restore cursor to file position in previous editing session
 vim.api.nvim_create_autocmd('BufReadPost', {
   callback = function(args)
+    local filetype = vim.api.nvim_get_option_value("filetype", { buf = args.buf })
+
+    if filetype == "snacks_picker_input" then
+      return
+    end
+
     local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
     local line_count = vim.api.nvim_buf_line_count(args.buf)
+
     if mark[1] > 0 and mark[1] <= line_count then
       vim.api.nvim_win_set_cursor(0, mark)
       -- Defer centering slightly so it's applied after render
