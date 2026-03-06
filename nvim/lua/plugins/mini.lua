06c3dc7186df42881d1980bf8fcd1036f3b978d5
Ben Verna-Asals <benjaminv@pixomondo.com>
2026-03-06 12:39:29 -0500

    Show relative path in statusline

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -8,61 +8,50 @@ return {
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
       require('mini.surround').setup()
 
-      local function filename_with_modified()
-        local filename = vim.fn.expand '%:t'
-        if filename == '' then filename = '[No Name]' end
+      local function filename_with_modified(show_relative_path)
+        local filename = vim.fn.expand(show_relative_path and '%:.' or '%:t')
 
-        local modified_marker = ''
-        if vim.bo.modified then modified_marker = ' [+]' end
+        if filename == '' then
+          filename = '[No Name]'
+        end
+
+        local modified_marker = vim.bo.modified and ' [+]' or ''
 
         return filename .. modified_marker
       end
 
-      -- Statusline
       local statusline = require 'mini.statusline'
-      statusline.setup { use_icons = vim.g.have_nerd_font }
+      statusline.setup {
+        use_icons = vim.g.have_nerd_font,
+        content = {
+          active = function()
+            local mode, mode_hl = statusline.section_mode { trunc_width = 120 }
+            local filename = filename_with_modified(true)
+            local fileinfo = statusline.section_fileinfo { trunc_width = 120 }
+            local location = statusline.section_location { trunc_width = 75 }
 
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_git = function()
-        return ''
-      end
+            return statusline.combine_groups {
+              { hl = mode_hl, strings = { mode } },
+              { hl = 'MiniStatuslineFilename', strings = { filename } },
+              '%=',
+              { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+              { hl = mode_hl, strings = { location } },
+            }
+          end,
 
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_diff = function()
-        return ''
-      end
+          inactive = function()
+            local filename = filename_with_modified(false)
+            local fileinfo = statusline.section_fileinfo { trunc_width = 120 }
+            local location = statusline.section_location { trunc_width = 75 }
 
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_diagnostics = function()
-        return ''
-      end
-
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_lsp = function()
-        return ''
-      end
-
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.inactive = function()
-        return statusline.section_filename {
-          { hl = 'MiniStatuslineFilenameInactive', strings = { filename_with_modified() } },
-        }
-      end
-
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_filename = function()
-        return filename_with_modified()
-      end
-
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_fileinfo = function()
-        return vim.bo.filetype
-      end
-
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
+            return statusline.combine_groups {
+              { hl = 'MiniStatuslineInactive', strings = { filename } },
+              '%=',
+              { hl = 'MiniStatuslineInactive', strings = { fileinfo, location } },
+            }
+          end,
+        },
+      }
     end,
   },
 }
